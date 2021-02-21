17aabe8be9593e2ad9a5610f5253f5d5acafcd09
bill-mansfield <manbil03@gmail.com>
2021-02-21 22:19:07 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MaterialLayout from '../components/Layout/MaterialLayout';
+import Base from '../components/Form/Base';
+
+function App() {
+  return (
+    <div>
+      <MaterialLayout>
+        <Base />
+      </MaterialLayout>
+    </div>
+  );
+}
+
+export default App;
